9dfdd073079f356bfe1db5a48bd16a4fc36f1d74
Marques Johansson <mjohansson@equinix.com>
2021-01-25 09:38:34 -0500

    demonstrate how to use a debug client
    Signed-off-by: Marques Johansson <mjohansson@equinix.com>

--- examples/plans/plans.go
+++ examples/plans/plans.go
@@ -1,29 +1,41 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
 	"os"
 
 	httptransport "github.com/go-openapi/runtime/client"
+	"github.com/go-openapi/strfmt"
 	"github.com/t0mk/gometal/client"
+	"sigs.k8s.io/yaml"
 )
 
+func NewDebugClient(formats strfmt.Registry, cfg *client.TransportConfig) *client.MetalAPI {
+	// ensure nullable parameters have default
+	if cfg == nil {
+		cfg = client.DefaultTransportConfig()
+	}
+
+	// create transport and client
+	transport := httptransport.New(cfg.Host, cfg.BasePath, cfg.Schemes)
+	transport.SetDebug(true)
+	return client.New(transport, formats)
+}
+
 func main() {
-	c := client.NewHTTPClient(nil)
+	c := NewDebugClient(nil, nil)
 	auth := httptransport.APIKeyAuth("X-Auth-Token", "header", os.Getenv("PACKET_AUTH_TOKEN"))
+
 	r, err := c.Plans.FindPlans(nil, auth)
 
 	if err != nil {
 		panic(err)
 	}
 
-	s, err := json.Marshal(r)
+	s, err := yaml.Marshal(r.GetPayload().Plans)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf(string(s))
-	log.Println(r)
+	fmt.Println(string(s))
 }
